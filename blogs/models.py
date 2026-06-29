7fc437e925ec26d656e1fc01350f6060479211f5
Akshita Khandelwal <khandelwalakshita041@gmail.com>
2026-06-29 16:28:37 +0530

    users model and blogs model - basic

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from users.models import Timestamps
+from django.conf import settings
 
 # Create your models here.
-class Blogs(models.Model):
-    pass
+class Blogs(Timestamps):
+    title = models.CharField(max_length=250, null=False)
+    content = models.TextField(blank=False, null=False)
+    
+    author_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blogs')
--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Timestamps(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+        
+class Users(AbstractUser, Timestamps):
+    username = None
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    
