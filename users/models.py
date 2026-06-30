7078840b31d701587ca4b9d3871b74a4ffbcdbe2
Akshita Khandelwal <khandelwalakshita041@gmail.com>
2026-06-30 18:15:50 +0530

    Custom manager

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from .managers import UserManager
 
 
 # Create your models here.
@@ -20,4 +21,7 @@ class Users(AbstractUser, Timestamps):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
     
+    def __str__(self):
+        return self.name
     
+    objects = UserManager()
