5281ddab335148f14fee84cc17a7444e33ee8a64
Akshita Khandelwal <khandelwalakshita041@gmail.com>
2026-07-23 15:44:21 +0530

    admin setup basic

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Blogs
 
 # Register your models here.
+
+@admin.register(Blogs)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "customize_title", "content", 
+    ) 
+    
+    def customize_title(self, obj):
+        if obj.title.islower():
+            return obj.title.capitalize()
+    
+        return obj.title
+
+    # @admin.display(ordering="content")
+    # clicking column will sort by specified name
+    def content_length(self, obj):
+        return "Content"
+    
+    # makes the columns clickable
+    list_display_links = (
+    "customize_title",
+)
